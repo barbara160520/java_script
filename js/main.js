d349a8309d8d13d8419c301472e3d30966f3dbed
barbara <barbara165@mail.ru>
2021-03-26 15:24:25 +0300

    corrected DZ

--- js/main.js
+++ js/main.js
@@ -6,19 +6,24 @@ const products = [
     {id: 4, title: 'Gamepad', price: 50},
 ];
 //Функция для формирования верстки каждого товара
-const renderProduct = (item) => {
+/*Для второго вариант в renderProduct нужно передвать item;
+в переменные записваем: item.id,item.title,.item.price*/
+const renderProduct = (id=5,title="PC Lenova",price=600) => {
     return `<div class="product-item">
-                <img src = https://picsum.photos/200/300?random=${item.id}" alt="prod">
-                <h3>${item.title}</h3>
-                <p>${item.price}$</p>
+                <img src = https://picsum.photos/200/300?random=${id}" alt="prod">
+                <h3>${title}</h3>
+                <p>${price}$</p>
                 <button class="buy-btn">Купить</button>
             </div>`
 };
 
 const renderPage = list => {
-    //const productsList = list.map(item => renderProduct(item.id,item.title, item.price));
-    const productsList = list.map(item => renderProduct(item));
+    const productsList = list.map(item => renderProduct(item.id,item.title, item.price));
+    //Второй вариант: сокращение записи функции
+    //const productsList = list.map(item => renderProduct(item));
     document.querySelector('.products').innerHTML = productsList.join("");
 };
 
 renderPage(products);
+//Добавление значения по умолчанию для аргументов функции
+document.querySelector('.products').insertAdjacentHTML('beforeend',renderProduct());
