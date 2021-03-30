9bf8097eee73a80e033177fd0098d0f0c2fb92d5
barbara <barbara165@mail.ru>
2021-03-30 18:25:37 +0300

    add DZ_2

--- js/main.js
+++ js/main.js
@@ -1,29 +1,95 @@
-'use strict'
-const products = [
-    {id: 1, title: 'Notebook', price: 2000},
-    {id: 2, title: 'Mouse', price: 20},
-    {id: 3, title: 'Keyboard', price: 200},
-    {id: 4, title: 'Gamepad', price: 50},
-];
-//Функция для формирования верстки каждого товара
-/*Для второго вариант в renderProduct нужно передвать item;
-в переменные записваем: item.id,item.title,.item.price*/
-const renderProduct = (id=5,title="PC Lenova",price=600) => {
-    return `<div class="product-item">
-                <img src = https://picsum.photos/200/300?random=${id}" alt="prod">
-                <h3>${title}</h3>
-                <p>${price}$</p>
+class ProductsList {
+    constructor(container = '.products') {
+        this.container = container;
+        this.goods = [];
+        this._fetchProducts();
+    }
+    _fetchProducts() {
+        this.goods = [
+            {
+                id: 1
+                , title: 'Notebook'
+                , price: 2000
+            }
+            , {
+                id: 2
+                , title: 'Mouse'
+                , price: 20
+            }
+            , {
+                id: 3
+                , title: 'Keyboard'
+                , price: 200
+            }
+            , {
+                id: 4
+                , title: 'Gamepad'
+                , price: 50
+            }
+        , ];
+    }
+    render() {
+        const block = document.querySelector(this.container);
+        for (let product of this.goods) {
+            const productObj = new ProductItem(product);
+            block.insertAdjacentHTML('beforeend', productObj.render())
+                //            block.innerHTML += productObj.render();
+        }
+    }
+    //Метод выводит сумму всех товаров
+    sumProducts() {
+        let money = 0;
+        const block = document.querySelector('.info');
+        for (let i = 0; i < this.goods.length; i++) {
+            money = money + this.goods[i].price;
+        }
+        block.insertAdjacentHTML('afterbegin', "Сумма всех товаров: " + money + " $");
+    }
+}
+class ProductItem {
+    constructor(product, img = 'https://placehold.it/200x150') {
+        this.title = product.title;
+        this.price = product.price;
+        this.id = product.id;
+        this.img = img;
+    }
+    render() {
+        return `<div class="product-item" data-id="${this.id}">
+                <img src="${this.img}" alt="Some img">
+                <h3>${this.title}</h3>
+                <p>${this.price}$</p>
                 <button class="buy-btn">Купить</button>
             </div>`
-};
+    }
+}
+//Класс для корзины товаров
+class Cart {
+    constructor(buy_btn = '.buy-btn') {
+        this.buy_btn = buy_btn;
+        this.cart_prod = [];
+    }
+    //Возможные методы
+    addItem() {
+        const block = document.querySelector('span');
+        let buy_btn = document.querySelectorAll('.buy-btn');
+        buy_btn.forEach(item => item.addEventListener("click", () => {
+            this.cart_prod.push(item.parentNode.getAttribute('data-id'));
+            block.innerHTML = 'В корзине: ' + this.cart_prod.length;
+        }));
+    }
+    deleteItem() {
+        const block = document.querySelector('span');
+        block.innerHTML = 'В корзине: 0';
+        for (let i = 0; i < this.cart_prod.length; i++) {
+            delete this.cart_prod[i];
+        }
+    }
+}
+//Класс для элемента корзины товаров
+class ItemCart {}
 
-const renderPage = list => {
-    const productsList = list.map(item => renderProduct(item.id,item.title, item.price));
-    //Второй вариант: сокращение записи функции
-    //const productsList = list.map(item => renderProduct(item));
-    document.querySelector('.products').innerHTML = productsList.join("");
-};
-
-renderPage(products);
-//Добавление значения по умолчанию для аргументов функции
-document.querySelector('.products').insertAdjacentHTML('beforeend',renderProduct());
+let list = new ProductsList();
+list.render();
+list.sumProducts();
+let prod = new Cart();
+prod.addItem();
