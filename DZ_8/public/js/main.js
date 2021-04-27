7e61cb415261268a401d58d8440578ef1cfb940f
barbara <barbara165@mail.ru>
2021-04-27 13:21:30 +0300

    finished DZ_8

---
+++ DZ_8/public/js/main.js
@@ -0,0 +1,121 @@
+const app = new Vue(
+    {
+        el: '#app',
+        data: {
+            products: [],
+            api: 'https://raw.githubusercontent.com/GeekBrainsTutorial/online-store-api/master/responses/',
+            cartData: '',     
+            showedProducts:[],
+            cartGoods:[],
+        },
+        methods: {
+            fetchProducts(url){
+                return fetch(url)
+                    .then(answer => answer.json())
+                    .catch(error =>{
+                        this.$refs.error.text = error;
+                    });
+            },
+            filterGoods(searchLine){
+                if (searchLine) {
+                    this.showedProducts = this.products.filter(value => value.product_name.toLowerCase().includes(searchLine.toLowerCase()))
+                } else {
+                    this.showedProducts = this.products;
+                }
+            },
+            postJson(url, data){
+                console.log(url)
+
+                return fetch(url, {
+                    method: 'POST',
+                    headers: {
+                        "Content-Type": "application/json"
+                    },
+                    body: JSON.stringify(data)
+                })
+                    .then(result => result.json())
+                    .catch(error => {
+                        this.$refs.error.text = error;
+                    })
+            },
+            putJson(url, data){
+                console.log(url)
+                return fetch(url, {
+                    method: 'PUT',
+                    headers: {
+                        "Content-Type": "application/json"
+                    },
+                    body: JSON.stringify(data)
+                })
+                    .then(result => result.json())
+                    .catch(error => {
+                        this.$refs.error.text = error;
+                    })
+            },
+            remove(url, data){
+                return fetch(url, {
+                    method: 'DELETE',
+                    headers: {
+                        "Content-Type": "application/json"
+                    },
+                    body: JSON.stringify(data)
+                })
+                    .then(result => {
+                         result.json()
+                         this.fetchProducts('/api/cart')
+                        .then(data => {
+                            console.log(data)
+                            this.cartGoods.push({amount: data.amount, countGoods: data.countGoods});
+                            this.cartGoods[1] = data.contents;
+                        });
+                    })
+                    .catch(error => {
+                        this.$refs.error.text = error;
+                    })
+            },
+            addProduct(item){
+                console.log(this.cartGoods[1])
+
+                let find = this.cartGoods[1].find(el => el.id_product === item.id_product);
+                if(find){
+                    this.putJson(`/api/cart/${find.id_product}`, {quantity: 1})
+                        .then(data => {
+                            if(data.result === 1){
+                                find.quantity++
+                            }
+                        })
+                } else {
+                    const prod = Object.assign({quantity: 1}, item);
+                    this.postJson(`/api/cart`, prod)
+                        .then(data => {
+                            if(data.result === 1){
+                                this.cartGoods[1].push(prod);
+                            }
+                        })
+                }
+            }
+        },
+        mounted() {
+            this.fetchProducts('/api/products')
+                .then(data => {
+                    this.products = [...data];
+                    this.showedProducts = this.products;
+                    for (let item of data){
+                        item.imgPath = `img/product_${item.id_product}.jpg`;
+                    }
+                })
+                .catch(error => {
+                    this.$refs.error.text = error;
+                });
+            this.fetchProducts('/api/cart')
+                .then(data => {
+                    this.cartGoods.push({amount: data.amount, countGoods: data.countGoods});
+                    this.cartGoods.push(data.contents);
+            })
+            .catch(error => {
+                this.$refs.error.text = error;
+            });
+            
+        },
+    }   
+)
